7772292f36c6fc9e098cdab5732e22904dcaefad
ImeshKavishka <154868013+imeshkavishka@users.noreply.github.com>
2025-07-11 16:10:16 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new typed(".text",{
+    Strings:["Frontend Developer","Backend Developer","Web Developer"],
+    typeSpeed : 100,
+    backspeed :100,
+    backDelay :1000,
+    loop :true
+
+});
+
